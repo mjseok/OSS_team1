b1bc0d4ead3b666c379788392ce8967348bbdc9d
seohyeon <sts05158@gmail.com>
2020-05-29 01:21:56 +0900

    COR:Correct global variable to local variable
    Move variable 'board_symbol[9]' to main().
    Use struct Player_Info* instead of global variable player_name and player_symbol.
    Fix parameters of chooseSymbol() to pass players information and change the information within the function.
    Fix parameters of checkHorizontal(), checkVertical(), checkDiagonal(), checkDraw() and showBoard() to pass board symbol.
    Fix parameters of showBoard() to pass players information.

--- Code.c
+++ Code.c
@@ -9,28 +9,38 @@ Author:- Mishal Shah
 
 #pragma warning (disable:4996)
 #define _CRT_SECURE_NO_WARNINGS
-char player1_symbol, player2_symbol; 
-char board_symbol[9]={'1','2','3','4','5','6','7','8','9'};
-char player1_name[50], player2_name[50]; 
-void showBoard(void);
+
+typedef struct
+{
+	char symbol;
+	char name[50];
+}Player_Info;
+
+
 void showRule(void);
-void chooseSymbol(void);
-int checkHorizontal(void);
-int checkVertical(void);
-int checkDiagonal(void); 
-int checkDraw(void);
+void chooseSymbol(Player_Info* game_player1, Player_Info* game_player2);
+int checkHorizontal(char game_board[]);
+int checkVertical(char game_board[]);
+int checkDiagonal(char game_board[]); 
+int checkDraw(char game_board[]);
+void showBoard(char game_board[], Player_Info game_player1, Player_Info game_player2);
 
 int main(void)
 {
     FILE *leaderboard;
-    leaderboard=fopen("leaderboard.txt","a+");
-    fclose(leaderboard);
-    system("color 09");
     int current_player=1;
-    int board_position,game_state=-1;
+    int board_position;
+    int game_state=-1;
     char symbol;
     int menu_option;
 
+    Player_Info player1;
+    Player_Info player2;
+    char board_symbol[9]={'1','2','3','4','5','6','7','8','9'};
+
+    leaderboard = fopen("leaderboard.txt","a+");
+    fclose(leaderboard);
+    system("color 09");
     showRule();
 
     printf("\n\nType 1 to start the game:-\nType 2 to view leader board:-\n");
@@ -41,36 +51,37 @@ read:
 	menu_option=1;
 	leaderboard=fopen("leaderboard.txt","a+");
     	printf("\nEnter name of player1: ");
-    	scanf("%s",player1_name);
-    	fprintf(leaderboard,"\n%s",player1_name);
+    	scanf("%s",player1.name);
+    	fprintf(leaderboard,"\n%s",player1.name);
     	printf("Enter name of player2: ");
-    	scanf("%s",player2_name);
-    	fprintf(leaderboard,"\t%s",player2_name);
+    	scanf("%s",player2.name);
+    	fprintf(leaderboard,"\t%s",player2.name);
     	fclose(leaderboard);
-    	if(!strcmp(player1_name, player2_name))
+
+    	if(!strcmp(player1.name, player2.name))
     	{
 	        printf("Enter names of different players!\n\n");
         	goto read;
     	}
    	 else
 
-        	chooseSymbol();
+        	chooseSymbol(&player1, &player2);
 
 
    	 system("color fc");
-   	 showBoard();
+   	 showBoard(board_symbol, player1, player2);
 
 
     	do
     	{
         	current_player=((current_player%2)?1:2);
         	if(current_player==1)
-        		printf("%s Type any digit from 1-9 to fill your response:- ",player1_name);
+        		printf("%s Type any digit from 1-9 to fill your response:- ",player1.name);
 
         	else
-            		printf("%s Type any digit from 1-9 to fill your response:- ",player2_name);
+            		printf("%s Type any digit from 1-9 to fill your response:- ",player2.name);
         	scanf("%d",&board_position);
-        	symbol=((current_player==1)? player1_symbol:player2_symbol);
+        	symbol=((current_player==1)? player1.symbol:player2.symbol);
         	if(board_position==1 && board_symbol[0]=='1')
             		board_symbol[0]=symbol;
         	else if(board_position==2 && board_symbol[1]=='2')
@@ -94,32 +105,34 @@ read:
 			printf("Wrong Selection\n");
 			current_player--;
 		}
-		game_state=checkVertical();
+
+		game_state=checkVertical(board_symbol);
 		if(game_state==1)
 		{
 			current_player++;
-			showBoard();
+			showBoard(board_symbol, player1, player2);
 			break;
 		}
-		game_state=checkHorizontal();
-		 if(game_state==1)
-		 {
+
+		game_state=checkHorizontal(board_symbol);
+		if(game_state==1)
+		{
                         current_player++;
-                        showBoard();
+                        showBoard(board_symbol, player1, player2);
 			break;
                 }
 
-		game_state=checkDiagonal();
-		 if(game_state==1)
-		 {
+		game_state=checkDiagonal(board_symbol);
+		if(game_state==1)
+		{
                         current_player++;
-                        showBoard();
+                        showBoard(board_symbol, player1, player2);
 			break;
                 }
 
-		game_state=checkDraw();
+		game_state=checkDraw(board_symbol);
         	current_player++;
-        	showBoard();
+        	showBoard(board_symbol, player1, player2);
     	}while(game_state == 0);
 
 
@@ -129,19 +142,22 @@ read:
 
         	if(current_player==2)
         	{
-			printf("\n\nPlayer1 %s Wins!\n\n",player1_name);fprintf(leaderboard,"\t%s",player1_name);
+			printf("\n\nPlayer1 %s Wins!\n\n",player1.name);
+			fprintf(leaderboard,"\t%s",player1.name);
         		getchar();
 		}
         	else
         	{
-			printf("\n\nPlayer2 %s Wins!\n\n",player2_name);fprintf(leaderboard,"\t%s",player2_name);
+			printf("\n\nPlayer2 %s Wins!\n\n",player2.name);
+			fprintf(leaderboard,"\t%s",player2.name);
 			getchar();
         	}
         	fclose(leaderboard);
     	}
     	else
 	{
-        	printf("\n\nGame Draws!\n\n");fprintf(leaderboard,"\t%s","DRAW");
+        	printf("\n\nGame Draws!\n\n");
+		fprintf(leaderboard,"\t%s","DRAW");
         	getchar();
 		fclose(leaderboard);
     	}
@@ -178,64 +194,71 @@ read:
 
    }
 }
-int checkHorizontal(void)
+
+
+int checkHorizontal(char game_board[])
 {
-    if(board_symbol[0]==board_symbol[1] && board_symbol[1]==board_symbol[2])
+    if(game_board[0] == game_board[1] && game_board[1] == game_board[2])
         return 1;
-    else if(board_symbol[3]==board_symbol[4] && board_symbol[4]==board_symbol[5])
+    else if(game_board[3] == game_board[4] && game_board[4] == game_board[5])
         return 1;
-    else if(board_symbol[6]==board_symbol[7] && board_symbol[7]==board_symbol[8])
+    else if(game_board[6] == game_board[7] && game_board[7] == game_board[8])
         return 1; 
     else
         return 0;
 }
-int checkVertical(void)
+
+
+int checkVertical(char game_board[])
 {
-    if(board_symbol[0]==board_symbol[3] && board_symbol[3]==board_symbol[6])
+    if(game_board[0] == game_board[3] && game_board[3] == game_board[6])
         return 1;
-    else if(board_symbol[1]==board_symbol[4] && board_symbol[4]==board_symbol[7])
+    else if(game_board[1] == game_board[4] && game_board[4] == game_board[7])
 	return 1;
-    else if(board_symbol[2]==board_symbol[5] &&board_symbol[5]==board_symbol[8])
+    else if(game_board[2] == game_board[5] && game_board[5] == game_board[8])
 	return 1;
     else
 	return 0;
 } 
-int checkDiagonal(void)
+
+int checkDiagonal(char game_board[])
 {
-    if(board_symbol[0]==board_symbol[4] && board_symbol[4]==board_symbol[8])
+    if(game_board[0] == game_board[4] && game_board[4] == game_board[8])
         return 1;
-    else if(board_symbol[2]==board_symbol[4] && board_symbol[4]==board_symbol[6])
+    else if(game_board[2] == game_board[4] && game_board[4] == game_board[6])
         return 1;
     else
         return 0;
 }
-int checkDraw(void)
+
+int checkDraw(char game_board[])
 {
-    if(board_symbol[0]!='1' && board_symbol[1]!='2' && board_symbol[2]!='3' && board_symbol[3]!='4' && board_symbol[4]!='5' && board_symbol[5]!='6' && board_symbol[6]!='7' && board_symbol[7]!='8' && board_symbol[8]!='9')
+    if(game_board[0] != '1' && game_board[1] != '2' && game_board[2] != '3' && game_board[3] !='4' && game_board[4] != '5' && game_board[5] != '6' && game_board[6] != '7' && game_board[7] != '8' && game_board[8] != '9')
         return -1;
     else
 	return 0;
 }
 
 
-void showBoard(void)
+void showBoard(char game_board[], Player_Info game_player1, Player_Info game_player2)
 {
 	system("cls");
     	printf("\tTic-Tac-Toe\n\n");
 
         printf("\n\n");
-        printf("%s:- (%c)\n%s:-  (%c)\n\n\n",player1_name,player1_symbol,player2_name,player2_symbol);
+        printf("%s:- (%c)\n%s:-  (%c)\n\n\n",game_player.name1,game_player1.symbol,game_player2.name,game_player2.symbol);
 
-        printf("  %c |  %c | %c\n",board_symbol[0],board_symbol[1],board_symbol[2]);
+        printf("  %c |  %c | %c\n", game_board[0], game_board[1], game_board[2]);
         printf("    |    |    \n");
         printf("----|----|----\n");
         printf("    |    |    \n");
-        printf("  %c |  %c | %c\n",board_symbol[3],board_symbol[4],board_symbol[5]);
+        printf("  %c |  %c | %c\n", game_board[3], game_board[4], game_board[5]);
         printf("    |    |    \n");
         printf("----|----|----\n");
-        printf("  %c |  %c | %c\n",board_symbol[6],board_symbol[7],board_symbol[8]);
+        printf("  %c |  %c | %c\n", game_board[6], game_board[7], game_board[8]);
         printf("    |    |    \n");
-    }
+}
+
 void showRule(void)
 {
     char link;
@@ -254,32 +277,32 @@ void showRule(void)
 
 }
 
-void chooseSymbol(void)
+void chooseSymbol(Player_Info* game_player1, Player_Info* game_player2)
 {
     char dec;
 deci:
-    printf("\n\nPlayer1 %s choose the X or 0:", player1_name);
+    printf("\n\nPlayer1 %s choose the X or 0:", game_player1->name);
     dec=getchar();
     scanf("%c",&dec);
     
         
-            if(dec=='X' || dec=='x')
-            {
-                player1_symbol='X';
-                player2_symbol='0';
-            }
-            else if(dec=='0')
-            {
-                player1_symbol='0';
-                player2_symbol='X';
-            }
-            else
-            {
-                printf("Please enter either X or 0 only \n\n");
-                goto deci;
-            }
+    if(dec=='X' || dec=='x')
+    {
+    	game_player1->symbol='X';
+    	game_player2->symbol='0';
+    }
+    else if(dec=='O' || dec=='o')
+    {
+    	game_player1->symbol='0';
+        game_player2->symbol='X';
+    }
+    else
+    {
+    	printf("Please enter either X or O only \n\n");
+        goto deci;
+    }
 
         
         
-    }
+}
 
