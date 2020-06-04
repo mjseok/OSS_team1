ef55f284e1182c0ffe9998493da496e338515dc7
mjseok98 <mjseok98@gmail.com>
2020-06-05 01:41:09 +0900

    ADD:Make UI function
    make 'startInterface' function for UI.
    Modify the code to improve the UI.
    
    resolve: #74

--- Code.c
+++ Code.c
@@ -14,6 +14,7 @@ typedef struct player_information
 	char symbol;
 	char name[11];
 }Player_Info;
+void startInterface(void);
 int checkLongName(void);
 int isNameSame(void);
 void inputName(void);
@@ -52,13 +53,13 @@ Player_Info player1 = { '\0','\0' };
 Player_Info player2 = { '\0','\0' };
 const int player1_turn = 1;
 const int player2_turn = 2;
-const int success =1;
+const int success = 1;
 const int failure = 0;
 
 
 int main(void)
 {
-	system("color 09");
+	startInterface();
 	showRule();
 	chooseMenu();
 }
@@ -67,17 +68,17 @@ int checkLongName(void) {
 	int length_player1Name = strlen(player1.name);
 	int length_player2Name = strlen(player2.name);
 
-	if (length_player1Name > max_length) 
+	if (length_player1Name > max_length)
 	{
 		return success;
 	}
 
-	else if (length_player2Name > max_length) 
+	else if (length_player2Name > max_length)
 	{
 		return success;
 	}
 
-	else 
+	else
 	{
 		return failure;
 	}
@@ -85,11 +86,11 @@ int checkLongName(void) {
 }
 
 int isNameSame(void) {
-	if (strcmp(player1.name, player2.name)==0) 
+	if (strcmp(player1.name, player2.name) == 0)
 	{
 		return success;
 	}
-	else 
+	else
 	{
 		return failure;
 	}
@@ -99,24 +100,29 @@ void inputName(void) {
 	enterName();
 	while (isNameSame())
 	{
-		printf("Enter names of different players!\n\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+		printf("\n\tEnter names of different players!\n\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+
 		enterName();
 	}
 
 	while (checkLongName())
 	{
-		printf("\nPlease enter name within 10 characters!\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+		printf("\n\tPlease enter name within 10 characters!\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 		enterName();
 	}
 }
 void enterName(void)
 {
-	printf("\nEnter name of player1: ");
+	printf("\n\tEnter name of player1: ");
 	scanf("%s", player1.name);
-	printf("Enter name of player2: ");
+	printf("\tEnter name of player2: ");
 	scanf("%s", player2.name);
 }
-void checkFile(FILE* file) 
+void checkFile(FILE* file)
 {
 	if (file == NULL)
 	{
@@ -151,27 +157,34 @@ void showResult(int game_state, int current_player)
 	const int someone_win = 1;
 	FILE* leaderboard = fopen("leaderboard.txt", "a+");
 	checkFile(leaderboard);
-	
+
 	fprintf(leaderboard, "%s\t%s\t", player1.name, player2.name);
 	if (game_state == someone_win)
 	{
 		if (current_player == player2_turn)
 		{
-			printf("\n\n %s Wins!\n\n", player1.name);
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+			printf("\n★★★Player1 %s Wins!★★★\n\n", player1.name);
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
 			fprintf(leaderboard, "%s\n", player1.name);
 		}
 		else
 		{
-			printf("\n\n %s Wins!\n\n", player2.name);
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+			printf("\n★★★Player2 %s Wins!★★★\n\n", player2.name);
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
 			fprintf(leaderboard, "%s\n", player2.name);
 		}
 	}
 	else
 	{
-		printf("\n\nGame Draws!\n\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+		printf("\n★★★Game Draws!★★★\n\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
 		fprintf(leaderboard, "%s\n", "DRAW");
 	}
 	fclose(leaderboard);
+
 }
 
 int isIndexValueSame(char game_board[], int index1, int index2, int index3)
@@ -281,59 +294,130 @@ void quit(void) {
 void showBoard(char game_board[])
 {
 	system("cls");
-	printf("\tTic-Tac-Toe\n\n");
-	printf("\n\n");
-	printf("%s:- (%c)\n%s:-  (%c)\n\n\n", player1.name, player1.symbol, player2.name, player2.symbol);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("〓〓〓〓〓〓〓〓〓〓〓〓〓\n");
+	printf("\tTic-Tac-Toe \n");
+	printf("〓〓〓〓〓〓〓〓〓〓〓〓〓\n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 13);
+	if (strcmp(player1.name, "\0") == 0)
+		printf("Player:- (0)\nComputer:-  (X)\n\n\n");
+	else
+		printf(" Player %s Symbol is (%c)\n Player %s Symbol is (%c)\n\n", player1.name, player1.symbol, player2.name, player2.symbol);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 
-	printf("  %c |  %c | %c\n", game_board[0], game_board[1], game_board[2]);
-	printf("    |    |    \n");
-	printf("----|----|----\n");
-	printf("    |    |    \n");
-	printf("  %c |  %c | %c\n", game_board[3], game_board[4], game_board[5]);
-	printf("    |    |    \n");
-	printf("----|----|----\n");
-	printf("  %c |  %c | %c\n", game_board[6], game_board[7], game_board[8]);
-	printf("    |    |    \n");
+	printf("■■■■■■■■■■■■■\n");
+	printf("■      ■      ■      ■\n");
+	printf("■      ■      ■      ■\n");
+	printf("■");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	printf("   %c", game_board[0]);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("  ■");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	printf("   %c", game_board[1]);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("  ■");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	printf("   %c", game_board[2]);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("  ■\n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("■      ■      ■      ■\n");
+	printf("■      ■      ■      ■\n");
+	printf("■■■■■■■■■■■■■\n");
+	printf("■      ■      ■      ■\n");
+	printf("■      ■      ■      ■\n");
+	printf("■");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	printf("   %c", game_board[3]);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("  ■");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	printf("   %c", game_board[4]);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("  ■");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	printf("   %c", game_board[5]);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("  ■\n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("■      ■      ■      ■\n");
+	printf("■      ■      ■      ■\n");
+	printf("■■■■■■■■■■■■■\n");
+	printf("■      ■      ■      ■\n");
+	printf("■      ■      ■      ■\n");
+	printf("■");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	printf("   %c", game_board[6]);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("  ■");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	printf("   %c", game_board[7]);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("  ■");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	printf("   %c", game_board[8]);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("  ■\n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("■      ■      ■      ■\n");
+	printf("■      ■      ■      ■\n");
+	printf("■■■■■■■■■■■■■\n\n");
+
+
+
 }
 
 void showRule(void)
 {
 	char more_rule[10];
-	printf("\tTic-Tac-Toe\n\n");
-	printf("Welcome to the most played 2D game and a sort of fun using X and O\n\n");
-	printf("Rules:-\n");
-	printf("\n1:Each player will be entering the number to put respective X or O in the desired position");
-	printf("\n2:Player who gets a combination of 3 same characters either diagonal or horizontally or \n  vertically will be declared as the winner");
-	printf("\n\nEnjoy the game! Be a Winner!\n\n");
-	printf("For more clarifications press Y else type any other character:- ");
+	printf("\tWelcome to the most played 2D game and a sort of fun using X and O\n\n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+
+	printf("\n\tFirst:Each player will be entering the number to put respective X or O in the desired position\n");
+	printf("\n\tSecond:Player who gets a combination of 3 same characters either diagonal or horizontally or \n\t  vertically will be declared as the winner");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+
+	printf("\n\n\tEnjoy the game! Be a Winner!\n\n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+
+	printf("\tFor more clarifications press Y else type any other character: ");
 	scanf("%[^\n]s", more_rule);
 	if (strcmp(more_rule, "y") == 0 || strcmp(more_rule, "Y") == 0)
 	{
 		system("start http://www.wikihow.com/Play-Tic-Tac-Toe");
 	}
+	system("cls");
 }
+
 
 void chooseSymbol(void)
 {
-	const int correct=1;
-	const int incorrect=0;
+	const int correct = 1;
+	const int incorrect = 0;
 	char get_symbol[10];
 	int menu_input = incorrect;
 	int right_input;
 
-	while (menu_input==incorrect) 
+	while (menu_input == incorrect)
 	{
-		printf("\n\n %s choose the X or O:", player1.name);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+		printf("\n\n\t%s choose the X or O:", player1.name);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
 		scanf("%s", get_symbol);
 		right_input = setSymbol(get_symbol);
-		if(right_input)
+		if (right_input)
 		{
-			menu_input=correct;
+			menu_input = correct;
 		}
 		else
 		{
-			printf("Please enter either X or O only \n\n");
-			menu_input=incorrect;
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+			printf("\n\tPlease enter either X or O only \n\n");
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+			menu_input = incorrect;
 		}
 	}
 }
@@ -360,9 +444,9 @@ int isRightInput(char* player_symbol, char* capital_letter, char* small_letter)
 int setSymbol(char* player1_symbol)
 {
 	int is_player_symbol;
-	
+
 	is_player_symbol = isRightInput(player1_symbol, "X", "x");
-	if(is_player_symbol)
+	if (is_player_symbol)
 	{
 		player1.symbol = 'X';
 		player2.symbol = 'O';
@@ -390,7 +474,6 @@ void playWithFriend(void) {
 
 	inputName();
 	chooseSymbol();
-	system("color fc");
 	showBoard(board_symbol);
 
 	while (game_state == keepGoing)
@@ -459,24 +542,30 @@ int checkSomeoneWin(char game_board[])
 		return game_continue;
 	}
 }
-
 void chooseMenu(void)
 {
 	const char* gotoTwoPlayerGame = "1";
 	const char* gotoComputerGame = "2";
 	const char* gotoLeaderBoard = "3";
 	const char* quitGame = "4";
-	const int correct=1;
-	const int incorrect=0;
-	
-	int menu_input= 0;
+	const int correct = 1;
+	const int incorrect = 0;
+
+	int menu_input = 0;
 	char menu_option[10];
 
-	while (menu_input==incorrect)
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+	printf("\n\t〓〓〓〓〓〓MENU〓〓〓〓〓〓");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+
+	while (menu_input == incorrect)
 	{
-		printf("\n\nPress 1 to play with friend\nPress 2 to play with computer\nPress 3 to show LeaderBoard\nPress 4 to quit Game\n--> ");
+		printf("\n\n\tPress 1 to play with friend\n\tPress 2 to play with computer\n\tPress 3 to show LeaderBoard\n\tPress 4 to quit Game\n\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+		printf("\tMenu_option: ");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 		scanf("%s", menu_option);
-		menu_input=correct;
+		menu_input = correct;
 
 		if (strcmp(menu_option, gotoTwoPlayerGame) == 0)
 		{
@@ -496,8 +585,10 @@ void chooseMenu(void)
 		}
 		else
 		{
-			printf("\nWrong insert. Press Again!");
-			menu_input=incorrect;
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+			printf("\n\tWrong insert. Press Again!");
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+			menu_input = incorrect;
 		}
 	}
 }
@@ -574,76 +665,144 @@ int checkComputer(char game_board[], char computer_symbol, char player_symbol)
 }
 void playWithComputer(void)
 {
-   char board_symbol[9] = { '1','2','3','4','5','6','7','8','9' };
-   char board_position[10];
-   const int keepGoing = 0;
-   int game_state = 0;
-   int current_player = 1;
-   int computer;
-   char player_symbol;
-   char computer_symbol;
+	char board_symbol[9] = { '1','2','3','4','5','6','7','8','9' };
+	char board_position[10];
+	const int keepGoing = 0;
+	int game_state = 0;
+	int current_player = 1;
+	int computer;
+	char player_symbol;
+	char computer_symbol;
 
-   strcpy(player1.name, "Player");
-   strcpy(player2.name, "Computer");
-   chooseSymbol();
-   system("color fc");
-   showBoard(board_symbol);
+	strcpy(player1.name, "Player");
+	strcpy(player2.name, "Computer");
+	chooseSymbol();
+	showBoard(board_symbol);
 
-   player_symbol = player1.symbol;
-   computer_symbol = player2.symbol;
+	player_symbol = player1.symbol;
+	computer_symbol = player2.symbol;
 
-   while (game_state == keepGoing)
-   {
-      if (current_player == player1_turn)
-      {
-	 checkTurn(current_player);
-         scanf("%s", board_position);
-         current_player = checkPosition(board_position, board_symbol, player_symbol, current_player);
-      }
-      else
-      {
-         computer = checkComputer(board_symbol, computer_symbol, player_symbol);
-         if (computer == 0)
-         {
-            computer = findBlankForComputer(board_symbol, computer_symbol, player_symbol);
-         }
-         board_symbol[computer] = computer_symbol;
-         printf("Computer select %d\n", computer + 1);
-         Sleep(1000);
-         current_player = changePlayer(current_player);
-      }
+	while (game_state == keepGoing)
+	{
+		if (current_player == player1_turn)
+		{
+			checkTurn(current_player);
+			scanf("%s", board_position);
+			current_player = checkPosition(board_position, board_symbol, player_symbol, current_player);
+		}
+		else
+		{
+			computer = checkComputer(board_symbol, computer_symbol, player_symbol);
+			if (computer == 0)
+			{
+				computer = findBlankForComputer(board_symbol, computer_symbol, player_symbol);
+			}
+			board_symbol[computer] = computer_symbol;
+			printf("Computer select %d\n", computer + 1);
+			Sleep(1000);
+			current_player = changePlayer(current_player);
+		}
 
-      game_state = checkSomeoneWin(board_symbol);
-      showBoard(board_symbol);
+		game_state = checkSomeoneWin(board_symbol);
+		showBoard(board_symbol);
 
-   }
+	}
 
-   showResult(game_state, current_player);
-	
+	showResult(game_state, current_player);
+
 }
 
 int checkPosition(char board_position[], char game_board[], char symbol, int current_player) {
-   char check_position[9][2] = { "1","2","3","4","5","6","7","8","9" };
-   int check_proper_position = 0;
-   const int proper_position = 1;
-   const int improper_position = 0;
-   for (int i = 0; i < 9; i++)
-   {
-      if (strcmp(check_position[i], board_position) == 0)
-      {
-         if (checkVacancies(i, game_board))
-         {
-            check_proper_position = proper_position;
-            game_board[i] = symbol;
-            current_player = changePlayer(current_player);
-            break;
-         }
-      }
-   }
+	char check_position[9][2] = { "1","2","3","4","5","6","7","8","9" };
+	int check_proper_position = 0;
+	const int proper_position = 1;
+	const int improper_position = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		if (strcmp(check_position[i], board_position) == 0)
+		{
+			if (checkVacancies(i, game_board))
+			{
+				check_proper_position = proper_position;
+				game_board[i] = symbol;
+				current_player = changePlayer(current_player);
+				break;
+			}
+		}
+	}
 
-   if (check_proper_position == improper_position) {
-      printf("Wrong Selection\n");
-      Sleep(1000);
-   }
-   return current_player;
+	if (check_proper_position == improper_position) {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+		printf("Wrong Selection\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+
+		Sleep(1000);
+	}
+	return current_player;
+}
+void startInterface(void) {
+
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("\t _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("\t|");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+	printf("-----------------------------------------------------------");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("| \n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("\t|");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	printf("-----------------------------------------------------------");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("| \n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("\t|");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+	printf("-----------------------------------------------------------");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("| \n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("\t|");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+	printf("======================");
+
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("TIc - Tac - Toe");
+
+
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 8);
+	printf("======================");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("| \n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("\t|");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+	printf("-----------------------------------------------------------");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("| \n");
+	printf("\t|");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+	printf("-----------------------------------------------------------");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("| \n"); printf("\t|");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+	printf("-----------------------------------------------------------");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("| \n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("\t|");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+	printf("------------------------------------------DESIGNED BY TEAM1");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("|\n");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+	printf("\t - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n");
+
+	printf("\n\n\n\t☆★☆★☆★☆★☆★☆★☆Game Rule☆★☆★☆★☆★☆★☆★☆\n\n");
+
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 }
