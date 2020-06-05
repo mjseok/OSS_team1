3c439e3e7b8bbcbbdee68aeb2d2cf02deb2673a3
cherry <cherry2030@naver.com>
2020-06-05 17:28:08 +0900

    COR:Use separate compilation
    Use separate compilation in playwithfriend.h.

---
+++ playwithfriend.h
@@ -0,0 +1,10 @@
+#ifndef __PLAYWITHFRIEND_H__
+#define __PLAYWITHFRIEND_H__
+
+int checkLongName(void);
+int isNameSame(void);
+void inputName(void);
+void enterName(void);
+void playWithFriend(void);
+
+#endif
