c65c8325db435f17003db9eba86e10afb3d858e6
cherry <cherry2030@naver.com>
2020-06-05 17:33:40 +0900

    COR:Change source file name
    Change Code.c to main.c.

---
+++ main.c
@@ -0,0 +1,59 @@
+/*
+
+This code has been compiled in Code::Blocks 16.01 IDE on Windows 10
+
+Author:- Mishal Shah
+
+*/
+
+#include <stdio.h>
+
+#include <string.h>
+
+#include <stdlib.h>
+
+#include <Windows.h>
+
+#include "declaration.h"
+
+#include "leaderboard.h"
+
+#include "ui.h"
+
+ 
+
+#include "playwithfriend.h"
+
+#include "playwithcomputer.h"
+
+#include "gameboard.h"
+
+#pragma warning (disable:4996)
+
+#define _CRT_SECURE_NO_WARNINGS
+
+ 
+
+const int player1_turn = 1;
+
+const int player2_turn = 2;
+
+ 
+
+Player_Info player1 = {'\0','\0'};
+
+Player_Info player2 = { '\0','\0' };
+
+const int success=1;
+
+const int failure=0;
+
+ 
+
+int main(void)
+
+{
+
+	startTicTacToe();
+
+}
