56aee897d7d0d8c5ef2fc0df3b3efe53351a1a69
seohyeon <sts05158@gmail.com>
2020-06-06 02:48:19 +0900

    COR:Remove unnecessary statements
    Remove the useless statements about the global variable in
    "declaration.h".

--- declaration.h
+++ declaration.h
@@ -7,12 +7,4 @@ typedef struct player_information
 	char name[11];
 }Player_Info;
 
-extern Player_Info player1;
-extern Player_Info player2;
-
-extern const int success;
-extern const int failure;
-extern const int player1_turn;
-extern const int player2_turn;
-
 #endif
